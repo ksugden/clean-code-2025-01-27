b40b46a50fc0714967fbe686432f75f8b05189bd
Kate Sugden <kate.sugden@madetech.com>
2025-01-28 13:08:47 +0000

    define Volume constants

--- volume.js
+++ volume.js
@@ -1,11 +1,15 @@
 export class Volume{
-    constructor(quantity, unit) {
+    constructor(quantity, volumeBaseUnit) {
         this.quantity = quantity
-        this.unit = unit
+        this.unit = volumeBaseUnit
     }
 
     equals(other){
         return this.quantity == other.quantity && this.unit == other.unit;
-
     }
 }
+
+
+export const TEASPOON = new Volume(1, new Volume())
+export const TABLESPOON = new Volume(3, TEASPOON)
+export const CUP = new Volume(3, TEASPOON)
--- volume.test.js
+++ volume.test.js
@@ -1,7 +1,7 @@
-import {Volume} from "./volume";
+import {Volume, TEASPOON} from "./volume";
 
 describe('Test volume...', () => {
     test('two equal volumes of the same unit are equal', () => {
-        expect(new Volume().equals(new Volume())).toBe(true);
+        expect(new Volume(1, TEASPOON).equals(new Volume(1, TEASPOON))).toBe(true);
     })
 })
