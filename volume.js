246500c0e06e4c08c2b3e5ab53651b0ddbf0a90b
Kate Sugden <kate.sugden@madetech.com>
2025-01-28 14:12:44 +0000

    add countBaseUnits

--- volume.js
+++ volume.js
@@ -1,15 +1,31 @@
 export class Volume{
-    constructor(quantity, volumeBaseUnit) {
+    constructor(quantity, volumeUnit) {
         this.quantity = quantity
-        this.volumeBaseunit = volumeBaseUnit
+        this.volumeUnit = volumeUnit
     }
 
+    countBaseUnits(){
+        let baseUnitCount = this.quantity
+        let volumeUnit = this.volumeUnit
+        while (volumeUnit) {
+            baseUnitCount = baseUnitCount * volumeUnit.quantity
+            volumeUnit = volumeUnit.volumeUnit
+        }
+        return baseUnitCount
+    }
+    
+    
     equals(other){
-        return this.quantity == other.quantity && this.volumeBaseunit == other.volumeBaseunit;
+        return this.countBaseUnits() == other.countBaseUnits()
     }
 }
+
 
 
 export const TEASPOON = new Volume(1)
 export const TABLESPOON = new Volume(3, TEASPOON)
 export const OZ = new Volume(2, TABLESPOON)
+
+// Volume(2, Volume(3, TEASPOONS)) = 3 TEASPOONS x 2 = (v1.quantity * v2.quantity , v2.unit)
+
+// Volume(2, Volume(4, Volume(5, Volume(1)))) = 2 * 4 * 5 * TEASPOONS
--- volume.test.js
+++ volume.test.js
@@ -1,7 +1,18 @@
-import {Volume, TEASPOON} from "./volume";
+import {Volume, TEASPOON, TABLESPOON, OZ} from "./volume";
 
 describe('Test volume...', () => {
     test('two equal volumes of the same unit are equal', () => {
         expect(new Volume(1, TEASPOON).equals(new Volume(1, TEASPOON))).toBe(true);
     })
+    test('two equal volumes of different unit are equal', () => {
+        expect(new Volume(1, TABLESPOON).equals(new Volume(3, TEASPOON))).toBe(true);
+    })
+    test('two equal volumes of different unit are equal', () => {
+        expect(new Volume(1, OZ).equals(new Volume(6, TEASPOON))).toBe(true);
+    })
 })
+
+// Volume(2, Volume(3, TEASPOONS)) = 3 TEASPOONS x 2 = (v1.quantity * v2.quantity , v2.unit)
+
+// Volume(2, Volume(4, Volume(5, Volume(1)))) = 2 * 4 * 5 * TEASPOONS
+
