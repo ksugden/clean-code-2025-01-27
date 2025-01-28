749b11131d41bb459ab5949159525e71cc255d70
Kate Sugden <kate.sugden@madetech.com>
2025-01-28 15:34:09 +0000

    Add an add method
    Co-authored-by: BushraAbdullahi <BushraAbdullahi@users.noreply.github.com>

--- volume.js
+++ volume.js
@@ -7,6 +7,7 @@ export class Volume{
     countBaseUnits(){
         let baseUnitCount = this.quantity
         let volumeUnit = this.volumeUnit
+
         while (volumeUnit) {
             baseUnitCount = baseUnitCount * volumeUnit.quantity
             volumeUnit = volumeUnit.volumeUnit
@@ -14,12 +15,20 @@ export class Volume{
         return baseUnitCount
     }
     
-    
     equals(other){
         return this.countBaseUnits() == other.countBaseUnits()
+    }
+
+    add(other){
+        const addition = other.countBaseUnits() + this.countBaseUnits()
+        return new Volume(addition, TEASPOON)
     }
 }
 
 export const TEASPOON = new Volume(1)
 export const TABLESPOON = new Volume(3, TEASPOON)
 export const OZ = new Volume(2, TABLESPOON)
+export const CUP = new Volume(8, OZ)
+export const PINT = new Volume(2, CUP)
+export const QUART = new Volume(2, PINT)
+export const GALLON = new Volume(4, QUART)
--- volume.test.js
+++ volume.test.js
@@ -1,6 +1,6 @@
 import {Volume, TEASPOON, TABLESPOON, OZ} from "./volume";
 
-describe('Test volume...', () => {
+describe('Test volumes are equal...', () => {
     test('two equal volumes of the same unit are equal', () => {
         expect(new Volume(1, TEASPOON).equals(new Volume(1, TEASPOON))).toBe(true);
     })
@@ -13,4 +13,11 @@ describe('Test volume...', () => {
     test('3 oz and 6 tablespoons are equal', () => {
         expect(new Volume(3, OZ).equals(new Volume(6, TABLESPOON))).toBe(true);
     })
+})
+
+describe('Add volumes...', () => {
+    test('Add two teaspoons', () => {
+        expect(new Volume(1, TEASPOON).add(new Volume(1, TEASPOON)).equals(new Volume(2, TEASPOON))).toBe(true);
+    })
+
 })
